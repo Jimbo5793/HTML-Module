c175b13b34830d71bbd1f939ee97e980bf94c2c3
Jimbo5793 <87202434+Jimbo5793@users.noreply.github.com>
2023-05-02 09:29:00 +0700

    Update script.js

--- HTML 2nd year/script.js
+++ HTML 2nd year/script.js
@@ -1,1 +1,40 @@
-alert("if you can see this, everything is working!")
+alert("if you can see this, everything should be working!")
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const maxSize = 20;
+const cellSize = Math.floor(canvas.width / maxSize);
+const grid = new Array(maxSize).fill(null).map(() => new Array(maxSize).fill(0));
+let start = null;
+let end = null;
+
+function drawEmpty(x, y) {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+function drawWall(x, y) {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+function drawStart(x, y) {
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+function drawEnd(x, y) {
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    grid.forEach((row, y) => {
+        row.forEach((cell, x) => {
+            grid[y][x] = 0;
+        });
+    });
+    start = null;
+    end = null;
+}
