4ac0ccbd13feeaeb757d9d28fe566f682b2f74a3
Aswadhpv <98012322+Aswadhpv@users.noreply.github.com>
2023-05-22 18:02:55 +0700

    script.js

--- HTML 2nd year/Ant/script.js
+++ HTML 2nd year/Ant/script.js
@@ -1,26 +1,73 @@
-var container = document.getElementById('container');
+// Define the canvas and context variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-function createAnt(id, x, y) {
-  var ant = document.createElement('div');
-  ant.id = 'ant_' + id;
-  ant.className = 'ant';
-  ant.style.left = x + 'px';
-  ant.style.top = y + 'px';
-  container.appendChild(ant);
+// Define the ant class
+class Ant {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
-function placeFood() {
-  var foodX = Math.floor(Math.random() * 480) + 10;
-  var foodY = Math.floor(Math.random() * 480) + 10;
-  
-  var food = document.createElement('div');
-  food.className = 'food';
-  food.style.left = foodX + 'px';
-  food.style.top = foodY + 'px';
-  container.appendChild(food);
+// Define the food class
+class Food {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 8, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
-// Example usage: create ants
-createAnt(1, 100, 100);
-createAnt(2, 200, 200);
-createAnt(3, 300, 300);
+// Create ants and food objects
+var ants = [
+  new Ant(100, 100),
+  new Ant(200, 200),
+  new Ant(300, 300)
+];
+var foods = [];
+
+// Function to draw the ants and food on the canvas
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw ants
+  ants.forEach(function(ant) {
+    ant.draw();
+  });
+
+  // Draw food
+  foods.forEach(function(food) {
+    food.draw();
+  });
+}
+
+// Function to handle mouse click event and add food at clicked location
+function addFood(event) {
+  var rect = canvas.getBoundingClientRect();
+  var x = event.clientX - rect.left;
+  var y = event.clientY - rect.top;
+  foods.push(new Food(x, y));
+  draw();
+}
+
+// Add event listener for mouse click to add food
+canvas.addEventListener("click", addFood);
+
+// Initial drawing
+draw();
